8ca69bae9fb283e958474b8bf23e9ede8b0fa086
Teddy (Gabby) <113390941+Appl3Kitti3@users.noreply.github.com>
2024-09-29 19:58:17 -0700

    Update server.js

--- server.js
+++ server.js
@@ -19,12 +19,14 @@ class Server
         let q = url.parse(req.url, true); // get the url information as string
         if (!q.query['text']) // If there is no ?text= in the url then read file
         {
-            fs.readFile(text.MYFILENAME, function(err, data) { // try to read file
+            const fileName = "." + q.pathname;
+            localStorage.setItem("path", fileName);
+            fs.readFile(fileName, function(err, data) { // try to read file
                 let statusCode = SUCCESS; // automatically set status code
                 let endResult = ""; // automatically set end result
 
                 if (err) { // if error create file with blank data
-                    MyFile.writeToFile(text.MYFILENAME, ""); // create file
+                    MyFile.writeToFile(fileName, ""); // create file
                     statusCode = NOTFOUND; // set status code to not found
                     endResult = `${q.pathname} ${text.NOTFOUNDMESSAGE}`; // set end result to not found message 
                 }
@@ -37,7 +39,7 @@ class Server
         }
         else
         {
-            MyFile.writeToFile(text.MYFILENAME, `${q.query['text']}${text.NEWLINE}`); // append the text to the file
+            MyFile.writeToFile(localStorage.getItem("path"), `${q.query['text']}${text.NEWLINE}`); // append the text to the file
             res.end(); // send the response
         }
         
@@ -58,7 +60,6 @@ class MyFile
     {
         // append the data to the file
         fs.appendFile(filename, data, function (err) {
-            console.log("AAAAAAAAAA");
         });
     }
 }
